0e81334a02ec8cc71d0f26d8719101bfc7709ea6
SangamithraS15707 <sangamithra15707@gmail.com>
2026-02-15 16:19:15 +0530

    first_version

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+print("Addition:",4+9)
+print("Subtraction:",4-9)
+print("Multiplication:",4*9)
+print("Division:",4/9)
+print("Exponentiation:",4**9)
+print("Floor Division:",4//9)
+print("Modulus:",4%9)
